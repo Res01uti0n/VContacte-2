86b7edf4a58bd198bc28a9504829efcd0ee3a3c4
Yaroslav Plotnickov <plotnickov.jaroslav@yandex.ru>
2020-03-24 21:40:00 +0300

    Created profile photos component

---
+++ src/components/shared/DropZoneInput.jsx
@@ -0,0 +1,31 @@
+import React, { useCallback } from "react"
+import { useDropzone } from "react-dropzone"
+
+import { Icon, Header } from "semantic-ui-react"
+
+const DropZoneInput = ({setFiles}) => {
+  const onDrop = useCallback(acceptedFiles => {
+    setFiles(acceptedFiles.map(file => {
+      return Object.assign(
+        file, { preview: URL.createObjectURL(file)}
+      )
+    }))
+  }, [setFiles])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({
+    onDrop, 
+    multiple: false, 
+    accept: "image/*"
+  })
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()}/>
+
+      <Icon name="upload" size="huge" />
+
+      <Header content="Drop image here" />
+    </div>
+  )
+}
+
+export default DropZoneInput
